3651671eb7f2d5a718a0eb22abd810e0ad15a4eb
botshen <363088847@qq.com>
2021-06-24 21:23:43 +0800

    添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,20 +5,20 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang="ts">
-import {inject, Ref} from 'vue';
-
+import { inject, Ref } from 'vue'
 export default {
-  setup() {
-    const menuVisible = inject<Ref<boolean>>('menuVisible');
-    const toggleMenu = () => {
-      menuVisible.value = !menuVisible.value;
-    };
-    return {toggleMenu};
+  setup(){
+    const menuVisible = inject<Ref<boolean>>('menuVisible') // get
+    const toggleMenu = ()=>{
+      menuVisible.value = !menuVisible.value
+    }
+    return {toggleMenu}
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .topnav {
@@ -27,20 +27,33 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
-
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
   }
-
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width:500px) {
+    > .menu{display: none;}
+    > .logo{margin: 0 auto;}
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.scss';
 import {createWebHashHistory, createRouter} from 'vue-router';
-
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
-    history: history,
+    history,
     routes: [
         {path: '/', component: Home},
         {path: '/doc', component: Doc}
     ]
 });
-
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
