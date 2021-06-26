09eaf1f5212ad8d8be2b2121196a62bc9ba7585c
botshen <363088847@qq.com>
2021-06-27 00:18:45 +0800

    支持 disabled

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -43,16 +43,23 @@
       <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
+  <h1>示例4</h1>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
 </template>
 <script lang="ts">
-import Button from "../lib/Button.vue";
+import Button from '../lib/Button.vue';
+
 export default {
-  components: { Button },
+  components: {Button},
   setup() {
     const onClick = () => {
-      console.log("hi");
+      console.log('hi');
     };
-    return { onClick };
+    return {onClick};
   },
 };
 </script>
