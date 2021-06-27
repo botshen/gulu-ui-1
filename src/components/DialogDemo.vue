386dc7bca65d210168a77ab17bdc27eab513ad12
botshen <363088847@qq.com>
2021-06-27 14:57:50 +0800

    Dialog 支持 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>OK</Button>
-      <Button>Cancel</Button>
-    </footer>
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x"></Dialog>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
+import {
+  ref
+} from 'vue'
 export default {
   components: {
+    Dialog,
     Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return {
+      x,
+      toggle
+    }
   }
 }
 </script>
