2d10c451fac7610b3d2a21c8855531f0d5444224
botshen <363088847@qq.com>
2021-06-28 23:16:39 +0800

    indicator css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,12 +2,11 @@
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
       <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item"
-                 :class="{selected:c.props.title===selected}"
-                 v-for="c in defaults"
-                 :is="c" />
+      <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+
     </div>
   </div>
 </template>
@@ -61,6 +60,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -72,12 +72,20 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
   &-content {
     padding: 8px 0;
-    &-item{
+    &-item {
       display: none;
-      &.selected{
+      &.selected {
         display: block;
       }
     }
