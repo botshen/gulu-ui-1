122e743917b2dc2b6dce2cc2f1d8d06072229f55
botshen <363088847@qq.com>
2021-07-02 09:29:51 +0800

    修复 selectedItem.value is null

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles"
-           :ref="el=>{if(el)navItems[index]=el}"
-           @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
@@ -16,8 +14,10 @@
 <script lang="ts">
 import Tab from './Tab.vue'
 import {
-  computed, onMounted, onUpdated, ref
-} from 'vue';
+  ref,
+  watchEffect,
+  onMounted
+} from 'vue'
 export default {
   props: {
     selected: {
@@ -25,31 +25,30 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems=ref<HTMLDivElement[]>([])
-    const indicator = ref<HTMLDivElement>(null)
-    const container = ref<HTMLDivElement>(null)
-    const x=()=>{
-      const divs = navItems.value
-      const result = divs.filter(div=>div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
-      indicator.value.style.width=width+'px'
-      const {left:left1} = container.value.getBoundingClientRect()
-      const {left:left2} = result.getBoundingClientRect()
-      const left = left2-left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
-    })
-    const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
     })
     const titles = defaults.map((tag) => {
       return tag.props.title
@@ -60,9 +59,8 @@ export default {
     return {
       defaults,
       titles,
-      current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     }
