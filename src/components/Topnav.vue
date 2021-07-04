2325dcfc3ed81369c97075daee3fd5403a173b5f
botshen <363088847@qq.com>
2021-07-04 21:46:10 +0800

    从 iconfont.cn 引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-king"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -8,20 +12,29 @@
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
+
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import {
+  inject,
+  Ref
+} from "vue";
 export default {
-  setup(){
-    const menuVisible = inject<Ref<boolean>>('menuVisible') // get
-    const toggleMenu = ()=>{
-      menuVisible.value = !menuVisible.value
-    }
-    return {toggleMenu}
-  }
-}
+  setup() {
+    const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return {
+      toggleMenu
+    };
+  },
+};
 </script>
+
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -31,19 +44,23 @@ export default {
   z-index: 10;
   justify-content: center;
   align-items: center;
-  > .logo {
+  >.logo {
     max-width: 6em;
     margin-right: auto;
+    >svg {
+      width: 32px;
+      height: 32px;
+    }
   }
-  > .menu {
+  >.menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-    > li {
+    >li {
       margin: 0 1em;
     }
   }
-  > .toggleAside {
+  >.toggleAside {
     width: 24px;
     height: 24px;
     background: red;
@@ -53,10 +70,16 @@ export default {
     transform: translateY(-50%);
     display: none;
   }
-  @media (max-width:500px) {
-    > .menu{display: none;}
-    > .logo{margin: 0 auto;}
-    > .toggleAside{display: inline-block;}
+  @media (max-width: 500px) {
+    >.menu {
+      display: none;
+    }
+    >.logo {
+      margin: 0 auto;
+    }
+    >.toggleAside {
+      display: inline-block;
+    }
   }
 }
 </style>
