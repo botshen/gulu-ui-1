0239fe319971c81ae824ff7e139315c46a48b707
shenxin <363088847@qq.com>
2021-07-07 21:43:22 +0800

    删掉无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,37 +1,24 @@
 <template>
   <div>
     <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1Demo" />
-    <Demo :component="Switch2Demo" />
+    <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
-import Switch1Demo from './Switch1.demo.vue'
-import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
-import Demo from './Demo.vue'
-import {
-  ref
-} from 'vue'
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+import Demo from './Demo.vue';
+
 export default {
   components: {
-    Button,
     Demo
   },
   setup() {
-    const bool = ref(false)
-    return {
-      bool,
-      Switch1Demo,
-      Switch2Demo,
-      Prism
-    }
+    return {Switch1Demo, Switch2Demo,};
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -39,22 +26,27 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-  >h2 {
+
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
+
   &-component {
     padding: 16px;
   }
+
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
+
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    >pre {
+
+    > pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
