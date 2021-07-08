1a30c6a80c26b7387aa0c66a8072430c3ecff404
shenxin <363088847@qq.com>
2021-07-08 23:45:24 +0800

    展示三点切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="topnav">
-    <div class="logo">
+    <router-link to="/" class="logo">
       <svg class="icon">
         <use xlink:href="#icon-king"></use>
       </svg>
-    </div>
+    </router-link>
     <ul class="menu">
       <li>
         <router-link to="/doc">文档</router-link>
       </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 
@@ -18,11 +20,16 @@
 import {
   inject,
   Ref
-} from 'vue';
-
+} from "vue";
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup() {
-    const menuVisible = inject<Ref<boolean>>('menuVisible'); // get
+    const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
     const toggleMenu = () => {
       menuVisible.value = !menuVisible.value;
     };
@@ -43,49 +50,43 @@ $color: #007974;
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 10;
+  z-index: 20;
   justify-content: center;
   align-items: center;
-
-  > .logo {
+  >.logo {
     max-width: 6em;
     margin-right: auto;
-
-    > svg {
+    >svg {
       width: 32px;
       height: 32px;
     }
   }
-
-  > .menu {
+  >.menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-
-    > li {
+    >li {
       margin: 0 1em;
     }
   }
-
-  > .toggleAside {
-    width: 24px;
-    height: 24px;
-    background: red;
+  >.toggleAside {
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
   }
-
   @media (max-width: 500px) {
-    > .menu {
+    >.menu {
       display: none;
     }
-    > .logo {
+    >.logo {
       margin: 0 auto;
     }
-    > .toggleAside {
+    >.toggleAside {
       display: inline-block;
     }
   }
