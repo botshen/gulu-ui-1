72bd73196b80b8ccb9ee0cae87d0994b8ca4155f
shenxin <363088847@qq.com>
2021-07-10 00:02:58 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="demo">
-    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <h2>{{component.__sourceCodeTitle}}</h2>
     <div class="demo-component">
-      <component :is="component"/>
+      <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="showCode" v-else>查看代码</Button>
       <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
-      <pre class="language-html" v-html="html"/>
+      <pre class="language-html" v-html="html" />
     </div>
 
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue';
+import Button from '../lib/Button.vue'
 import 'prismjs';
-import 'prismjs/themes/prism.css';
-import {computed, ref} from 'vue';
-
-const Prism = (window as any).Prism;
+import 'prismjs/themes/prism.css'
+import {
+  computed,
+  ref
+} from 'vue';
+const Prism = (window as any).Prism
 export default {
   components: {
     Button
@@ -31,17 +33,20 @@ export default {
   },
   setup(props) {
     const html = computed(() => {
-      return Prism.highlight
-      (props.component.__sourceCode, Prism.languages.html, 'html');
-    });
-    const codeVisible = ref(false);
-    const showCode = () => codeVisible.value = true;
-    const hideCode = () => codeVisible.value = false;
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+    })
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
+    const codeVisible = ref(false)
     return {
-      Prism, html, codeVisible, showCode,hideCode
-    };
+      Prism,
+      html,
+      codeVisible,
+      showCode,
+      hideCode
+    }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -49,27 +54,22 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-
-  > h2 {
+  >h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
-
   &-component {
     padding: 16px;
   }
-
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
-
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-
-    > pre {
+    >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
