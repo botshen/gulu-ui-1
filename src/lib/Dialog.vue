d9adbddb06563357d698b48aee5524628cca023e
shenxin <363088847@qq.com>
2021-07-10 23:11:53 +0800

    test

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -5,11 +5,11 @@
       <div class="gulu-dialog-wrapper">
         <div class="gulu-dialog">
           <header>
-            <slot name="title" />
+            <slot name="title"/>
             <span @click="close" class="gulu-dialog-close"></span>
           </header>
           <main>
-            <slot name="content" />
+            <slot name="content"/>
           </main>
           <footer>
             <Button level="main" @click="ok">OK</Button>
@@ -22,7 +22,8 @@
 </template>
 
 <script lang="ts">
-import Button from "./Button.vue";
+import Button from './Button.vue';
+
 export default {
   props: {
     visible: {
@@ -45,28 +46,28 @@ export default {
   },
   setup(props, context) {
     const close = () => {
-      context.emit('update:visible', false)
-    }
+      context.emit('update:visible', false);
+    };
     const onClickOverlay = () => {
       if (props.closeOnClickOverlay) {
-        close()
+        close();
       }
-    }
+    };
     const ok = () => {
-      if (props.ok?.() !== false) {
-        close()
+      if (props.ok && props.ok() !== false) {
+        close();
       }
-    }
+    };
     const cancel = () => {
-      props.cancel?.()
-      close()
-    }
+      props.cancel && props.cancel();
+      close();
+    };
     return {
       close,
       onClickOverlay,
       ok,
       cancel
-    }
+    };
   }
 };
 </script>
@@ -80,6 +81,7 @@ $border-color: #d9d9d9;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -89,6 +91,7 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -96,7 +99,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -104,20 +108,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: '';
@@ -128,9 +136,11 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
