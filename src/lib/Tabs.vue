5850f14c8a14a6a7ca4339fe42ad92951d2cc903
shenxin <363088847@qq.com>
2021-07-11 11:40:04 +0800

    修复 Tabs 在 Vue 3 正式版的 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }"
+           @click="select(t)" :class="{selected: t=== selected}" :key="index">{{ t }}
+      </div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+      <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
+                 :is="c"/>
 
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
 import {
   ref,
   watchEffect,
   onMounted
-} from 'vue'
+} from 'vue';
+
 export default {
   props: {
     selected: {
@@ -25,38 +29,38 @@ export default {
     }
   },
   setup(props, context) {
-    const selectedItem = ref < HTMLDivElement > (null)
-    const indicator = ref < HTMLDivElement > (null)
-    const container = ref < HTMLDivElement > (null)
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
     onMounted(() => {
       watchEffect(() => {
         const {
           width
-        } = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width + 'px'
+        } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + 'px';
         const {
           left: left1
-        } = container.value.getBoundingClientRect()
+        } = container.value.getBoundingClientRect();
         const {
           left: left2
-        } = selectedItem.value.getBoundingClientRect()
-        const left = left2 - left1
-        indicator.value.style.left = left + 'px'
-      })
-    })
-    const defaults = context.slots.default()
+        } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + 'px';
+      }, {flush: 'post'});
+    });
+    const defaults = context.slots.default();
     defaults.forEach((tag) => {
       // @ts-ignore
       if (tag.type.name !== Tab.name) {
-        throw new Error('Tabs 子标签必须是 Tab')
+        throw new Error('Tabs 子标签必须是 Tab');
       }
-    })
+    });
     const titles = defaults.map((tag) => {
-      return tag.props.title
-    })
+      return tag.props.title;
+    });
     const select = (title: string) => {
-      context.emit('update:selected', title)
-    }
+      context.emit('update:selected', title);
+    };
     return {
       defaults,
       titles,
@@ -64,9 +68,9 @@ export default {
       selectedItem,
       indicator,
       container
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -79,17 +83,21 @@ $border-color: #d9d9d9;
     color: $color;
     border-bottom: 1px solid $border-color;
     position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
     &-indicator {
       position: absolute;
       height: 3px;
@@ -100,10 +108,13 @@ $border-color: #d9d9d9;
       transition: all 250ms;
     }
   }
+
   &-content {
     padding: 8px 0;
+
     &-item {
       display: none;
+
       &.selected {
         display: block;
       }
